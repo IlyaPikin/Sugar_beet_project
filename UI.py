caf81c7c59b8bbf1f3e3927c7c4fa719439fb309
YoungPinch <dreamerandrew17@gmail.com>
2023-04-13 15:32:53 +0300

    v2

--- UI.py
+++ UI.py
@@ -1,15 +1,63 @@
+import random
 from tkinter import *
 from tkinter import ttk     # подключаем пакет ttk
 from tkinter import filedialog
+import numpy as np
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
+NavigationToolbar2Tk)
+
 ###################### Настройки #############################
+
 width_screen = 1100
 height_screen = 720
 
+###################### Создание окна #######################
+
+root = Tk()
+root.title("Sugar Beet")
+root.geometry("{0}x{1}+350+200".format(width_screen, height_screen))
+root.resizable(False, False) # для того, чтобы запретить растягивание
+
+################ Создание фреймов ############################
+
+top_frame = ttk.Frame(master=root,
+                      width=width_screen,
+                      height=height_screen*0.2,
+                      borderwidth=1,
+                      relief=SOLID,
+                      padding=20
+                      )
+
+matrix_frame = ttk.Frame(master=root,
+                         width=500,
+                         height=500
+                         )
+
+result_frame = ttk.Frame(master=root,
+                      width=500,
+                      height=500
+                      )
+
+top_frame.pack(anchor='n', fill=BOTH)
+matrix_frame.pack(side=LEFT)
+result_frame.pack(side=RIGHT)
 
 ##################### Переменные состояния ####################
+
 n_choice = 10
+fields_matrix = [] # В ней хранятся инпут поля гуя
+matrix = [] # полноценная матрица
+matrix_show_button_onscreen = False
+showed_matrix_fields = False
+is_generated_by_random= False
 
 ###################### Шаблоны функций ########################
+
+def dismiss(window):
+    window.grab_release()
+    window.destroy()
+
 def load_matrix_from_file():
     filepath = filedialog.askopenfilename()
 
@@ -19,8 +67,16 @@ def save_res_in_table():
 def save_res_in_graph():
     filepath = filedialog.asksaveasfilename()
 
-def gen_random_matr(n_choice):
-    pass
+def gen_random_matr():
+    global is_generated_by_random
+    is_generated_by_random=True
+    fields_matrix.clear()
+    for row in range(n_choice):
+        for column in range(n_choice):
+            value = DoubleVar(value=random.randint(10, 30))
+            fields_matrix.append(value)
+    display_matrix(n_choice, matrix_frame)
+
 
 def start_experiments():
     pass
@@ -31,11 +87,115 @@ def change_borders():
 def calculate():
     pass
 
-###################### Создание окна #######################
-root = Tk()
-root.title("Sugar Beet")
-root.geometry("{0}x{1}+350+200".format(width_screen, height_screen))
-root.resizable(False, False) # для того, чтобы запретить растягивание
+def get_row_matrix(row):
+    result = ""
+    for i in range(n_choice):
+        result += "{0} ".format(fields_matrix[row*n_choice + i].get())
+    return result
+
+def show_matrix():
+    window = Toplevel()
+    window.title("Матрица")
+    window.geometry("400x400")
+    window.protocol("WM_DELETE_WINDOW", lambda: dismiss(window))
+    for row in range(n_choice):
+        label = ttk.Label(master=window, text=get_row_matrix(row))
+        label.pack(anchor="nw")
+
+
+
+show_matr_button = ttk.Button(master=matrix_frame,
+                           text="Показать матрицу",
+                           command=show_matrix)
+
+show_matr_button.place(x=250, y=200)
+
+def destroy_matrix():
+    global fields_matrix
+    for value in fields_matrix:
+        value.destroy()
+    fields_matrix.clear()
+
+
+def display_matrix(n_choice, frame):
+
+    global matrix_frame
+    global matrix_show_button_onscreen
+    global showed_matrix_fields
+    global show_matr_button
+    global is_generated_by_random
+
+    if n_choice > 5:
+        if not matrix_show_button_onscreen:
+            matrix_frame.destroy()
+            matrix_frame = ttk.Frame(master=root,
+                                     width=500,
+                                     height=500
+                                     )
+            matrix_frame.pack(side=LEFT)
+            show_matr_button = ttk.Button(master=matrix_frame,
+                           text="Показать матрицу",
+                           command=show_matrix)
+
+            show_matr_button.place(x=250, y=200)
+            matrix_show_button_onscreen = True
+            showed_matrix_fields = False
+    else:
+        if is_generated_by_random:
+            is_generated_by_random = False
+            matrix_frame.destroy()
+            matrix_frame = ttk.Frame(master=root,
+                                     width=500,
+                                     height=500
+                                     )
+            matrix_frame.place(x=60, y=250)
+            matrix_show_button_onscreen = False
+            showed_matrix_fields = True
+            for row in range(n_choice):
+                for column in range(n_choice):
+                    field = ttk.Entry(master=matrix_frame,
+                                      width=15,
+                                      textvariable=fields_matrix[row * n_choice + column],
+                                      justify=CENTER)
+                    field.grid(row=row, column=column)
+        elif matrix_show_button_onscreen:
+            show_matr_button.destroy()
+            matrix_frame.destroy()
+            matrix_frame = ttk.Frame(master=root,
+                                     width=500,
+                                     height=500
+                                     )
+            matrix_frame.place(x=60, y=250)
+            matrix_show_button_onscreen = False
+            showed_matrix_fields = True
+            for row in range(n_choice):
+                for column in range(n_choice):
+                    value = DoubleVar()
+                    field = ttk.Entry(master=matrix_frame,
+                                      width=15,
+                                      textvariable=value,
+                                      justify=CENTER)
+                    fields_matrix.append(value)
+                    field.grid(row=row, column=column)
+        else:
+            matrix_show_button_onscreen = False
+            matrix_frame.destroy()
+            show_matr_button.destroy()
+            matrix_frame.destroy()
+            matrix_frame = ttk.Frame(master=root,
+                                     width=500,
+                                     height=500
+                                     )
+            matrix_frame.place(x=60, y=250)
+            for row in range(n_choice):
+                for column in range(n_choice):
+                    value = DoubleVar()
+                    field = ttk.Entry(master=matrix_frame,
+                                      width=15,
+                                      textvariable=value,
+                                      justify=CENTER)
+                    fields_matrix.append(value)
+                    field.grid(row=row, column=column)
 
 
 
@@ -46,14 +206,12 @@ file_menu = Menu(tearoff=0)
 save_menu = Menu(tearoff=0)
 setting_menu = Menu(tearoff=0)
 
-save_menu.add_command(label="Table", command=save_res_in_table)
-save_menu.add_command(label="Graphic", command=save_res_in_graph)
+save_menu.add_command(label="Сохранить таблицу погрешностей", command=save_res_in_table)
+save_menu.add_command(label="Сохранить ", command=save_res_in_graph)
 
-file_menu.add_command(label="Load matrix", command=load_matrix_from_file)
+file_menu.add_command(label="Загрузить матрицу из файла", command=load_matrix_from_file)
 
-def dismiss(window):
-    window.grab_release()
-    window.destroy()
+
 
 
 a_min = DoubleVar(value=0.5)
@@ -87,41 +245,15 @@ def border_screen():
     close_button.pack(anchor="center", expand=1)
     window.grab_set()
 
-setting_menu.add_command(label="Change borders", command=border_screen)
+setting_menu.add_command(label="Изменить границы генерации", command=border_screen)
 
-file_menu.add_cascade(label="Save", menu=save_menu)
-main_menu.add_cascade(label="File", menu=file_menu)
-main_menu.add_cascade(label="Setting", menu=setting_menu)
+file_menu.add_cascade(label="Сохранить...", menu=save_menu)
+main_menu.add_cascade(label="Файл", menu=file_menu)
+main_menu.add_cascade(label="Настройки генерации", menu=setting_menu)
 root.config(menu=main_menu)
 
 
-################ Создание фреймов ############################
 
-top_frame = ttk.Frame(master=root,
-                      width=width_screen,
-                      height=height_screen*0.2,
-                      borderwidth=1,
-                      relief=SOLID,
-                      padding=20
-                      )
-
-matrix_frame = ttk.Frame(master=root,
-                      width=width_screen / 2,
-                      height=height_screen*0.8,
-                      borderwidth=1,
-                      relief=SOLID,
-                      )
-
-result_frame = ttk.Frame(master=root,
-                      width=width_screen / 2,
-                      height=height_screen*0.8,
-                      borderwidth=1,
-                      relief=SOLID,
-                      )
-
-top_frame.pack(anchor='n', fill=X)
-matrix_frame.pack(anchor='w')
-result_frame.pack(anchor='e')
 
 ######################## Заполнение 1 фрейма ###############################
 
@@ -134,6 +266,12 @@ def change_choice(newVal):
     label_n_choice["text"] = "Размерность матрицы: {0}".format(int_value)
     global n_choice
     n_choice = int_value
+    fields_matrix.clear()
+    for row in range(n_choice):
+        for column in range(n_choice):
+            fields_matrix.append(DoubleVar())
+    display_matrix(n_choice, matrix_frame)
+
 
 horizontalScale = ttk.Scale(master=top_frame,
                             orient=HORIZONTAL,
@@ -146,20 +284,22 @@ label_n_choice.pack(anchor='nw')
 horizontalScale.pack(anchor='w')
 
 random_button = ttk.Button(master=top_frame,
-                         text="Random",
-                         command=gen_random_matr(n_choice),
-                         width=15
+                         text="Сгенерировать матрицу",
+                         command=gen_random_matr,
+                         width=25
                          )
 
-random_button.place(x=550, y=15)
+x_coord_first_button = 470
+
+random_button.place(x=x_coord_first_button, y=15)
 
 experiments_button = ttk.Button(master=top_frame,
-                         text="Experiment",
+                         text="Провети эксперимент",
                          command=start_experiments,
-                         width=15
+                         width=25
                          )
 
-experiments_button.place(x=550 + 125, y=15)
+experiments_button.place(x=x_coord_first_button + 170, y=15)
 
 
 is_venger = IntVar()
@@ -188,22 +328,52 @@ def choice_screen():
     window.grab_set()
 
 choice_algs_button = ttk.Button(master=top_frame,
-                         text="Choice Algs",
+                         text="Выбрать алгоритмы",
                          command=choice_screen,
-                         width=15
+                         width=20
                          )
 
-choice_algs_button.place(x=675 + 125, y=15)
+choice_algs_button.place(x=x_coord_first_button + 170+170, y=15)
 
 calculate_button = ttk.Button(master=top_frame,
-                         text="Calculate",
+                         text="Посчитать",
                          command=calculate,
                          width=15
                          )
 
-calculate_button.place(x=925, y=15)
+calculate_button.place(x=x_coord_first_button + 170+170 + 140, y=15)
 
-############################ Frame 2 ###############################
 
+################################### left frame #############################
+def plot(window):
+    fig = Figure(figsize=(5, 5),
+                 dpi=100)
+
+    # list of squares
+    y = [i ** 2 for i in range(101)]
+
+    # adding the subplot
+    plot1 = fig.add_subplot(111)
+
+    # plotting the graph
+    plot1.plot(y)
+    canvas = FigureCanvasTkAgg(fig,
+                               master=result_frame)
+    canvas.draw()
+
+    # placing the canvas on the Tkinter window
+    canvas.get_tk_widget().pack()
+
+    # creating the Matplotlib toolbar
+    toolbar = NavigationToolbar2Tk(canvas,
+                                   result_frame)
+    toolbar.update()
+
+    # placing the toolbar on the Tkinter window
+    canvas.get_tk_widget().pack()
+
+plot(result_frame)
+result_label = ttk.Label(master=result_frame, text="Результат: ")
+result_label.pack()
 
 root.mainloop()
